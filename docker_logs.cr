e9ef045230214b5334642b96da034d29b453a3b3
Lampros Chaidas <lampros@chaidas.com>
2018-11-17 20:27:59 -0800

    switched to TOML settings,split the functions into startup functions and docker specific functions

--- docker_logs.cr
+++ docker_logs.cr
@@ -1,54 +1,78 @@
 require "db"
 require "pg" #postgres
 require "json"
-require "poncho/parser" #reads .env files
+require "toml" #reads our configuration files
+require "./startup_checks.cr"
 require "./docker_logs_startup_checks.cr"
-
-# include functions from the ./docker_logs_startup_checks.cr file
-include DockerLogsStartupChecks
 
 OK   = "[  OK  ]"
 INFO = "[ INFO ]"
 FAIL = "[ FAIL ]"
-SETTINGS_FILE = "./settings"
+SETTINGS_FILE = "./settings.toml"
 
-# the check_if_settings_file_exists function is in ./docker_logs_startup_checks.cr
+##########################################################################################
+# Include functions from files
+##########################################################################################
+
+# include functions from the ./startup_checks.cr file
+include StartupChecks
+
+# include functions from the ./docker_logs_startup_checks.cr file
+include DockerLogsStartupChecks
+
+##########################################################################################
+# Startup checks
+##########################################################################################
+
+# the check_if_settings_file_exists function is in ./startup_checks.cr
 check_if_settings_file_exists(SETTINGS_FILE)
 
-puts "#{INFO} - Parsing \"#{SETTINGS_FILE}\"..."
-PG_SETTINGS = Poncho.from_file SETTINGS_FILE
+puts "#{INFO} - Parsing \"#{SETTINGS_FILE}\" - \"settings\" table..."
+SETTINGS    = TOML.parse_file(SETTINGS_FILE)["settings"].as(Hash)
+PG_SETTINGS = TOML.parse_file(SETTINGS_FILE)["pg_settings"].as(Hash)
+LOG_FILES   = TOML.parse_file(SETTINGS_FILE)["log_files"].as(Hash)
 
-# the check_for_shell_command function is in ./docker_logs_startup_checks.cr
-check_for_shell_command("docker")
+# the check_for_shell_command function is in ./startup_checks.cr
 check_for_shell_command("logtail2")
 
-# the get_hostname function is in ./docker_logs_startup_checks.cr
+# the get_hostname function is in ./startup_checks.cr
 HOSTNAME = get_hostname
 
-# the check_if_the_docker_service_is_running function is in ./docker_logs_startup_checks.cr
-check_if_the_docker_service_is_running
+##########################################################################################
+# Docker logs startup checks
+##########################################################################################
 
-# the get_docker_root_dir function is in ./docker_logs_startup_checks.cr
-DOCKER_ROOT_DIR=get_docker_root_dir
+if SETTINGS["PROCESS_DOCKER_LOGS"] == true
 
-# before we go any further let's make sure that we did get a value for the docker root directory
-puts "#{INFO} - Checking if we got the docker root directory..."
-if DOCKER_ROOT_DIR.empty?
-	puts "#{FAIL} - Sorry, I couldn't get the docker root directory"
-	exit 1
+	check_for_shell_command("docker")
+
+	# the check_if_the_docker_service_is_running function is in ./docker_logs_startup_checks.cr
+	check_if_the_docker_service_is_running
+
+	# the get_docker_root_dir function is in ./docker_logs_startup_checks.cr
+	docker_Root_Dir=get_docker_root_dir
+
+	# before we go any further let's make sure that we did get a value for the docker root directory
+	puts "#{INFO} - Checking if we got the docker root directory..."
+	if docker_Root_Dir.empty?
+		puts "#{FAIL} - Sorry, I couldn't get the docker root directory"
+		exit 1
+	else
+		puts "#{OK} - I got the docker root directory: \"#{docker_Root_Dir}\""
+	end
+
 else
-	puts "#{OK} - I got the docker root directory: \"#{DOCKER_ROOT_DIR}\""
+	puts "#{INFO} - The \"PROCESS_DOCKER_LOGS\" option is set to \"false\" so I will not process any docker logs"
 end
 
-puts "#{INFO} - Get list of containers..."
-containers=`docker ps --all --format "{{.ID}} {{.Names}}"`.strip.split("\n")
-if $?.exit_status != 0
-	puts "#{FAIL} - Sorry, something went wrong while trying to get the container listing"
-	exit 1
-else
-	puts "#{OK} - Got #{containers.size} containers."
-end
+##########################################################################################
+# Processing lines
+##########################################################################################
 
+# takes in a log file and the name of a docker container
+# returns an array that includes:
+# the temporary filename that contains the new lines (string)
+# the number of those lines (integer)
 def get_new_lines_from_file(log_filename,name)
 	log_basename=File.basename(log_filename)
 	`logtail2 -f #{log_filename} -o /dev/shm/#{log_basename}.offset > /tmp/#{log_basename}.tmp`
@@ -62,6 +86,8 @@ def get_new_lines_from_file(log_filename,name)
 	end
 end
 
+# this function takes the temporary filename created by logtail2 and 
+# processes it line by line, adding lines to the database in a table
 def process_file(file_to_process, lines_to_process, db, table_name)
 	counter=0
 	start_cycle=Time.now.to_unix_f
@@ -85,7 +111,7 @@ PG_USERNAME = PG_SETTINGS["PG_USERNAME"]
 PG_PASSWORD = PG_SETTINGS["PG_PASSWORD"]
 PG_DATABASE = PG_SETTINGS["PG_DATABASE"]
 PG_HOSTNAME = PG_SETTINGS["PG_HOSTNAME"]
-PG_PORT     = PG_SETTINGS["PG_PORT"].to_i
+PG_PORT     = PG_SETTINGS["PG_PORT"]
 
 begin
 	db = DB.open "postgres://#{PG_USERNAME}:#{PG_PASSWORD}@#{PG_HOSTNAME}:#{PG_PORT}/#{PG_DATABASE}"
@@ -108,34 +134,50 @@ else
 	end
 end
 
-puts "#{INFO} - Getting the container logs..."
-containers.each do |container|
-	id         = container.split(" ")[0]
-	name       = container.split(" ")[1]
-	full_id    = `docker inspect --format="{{.Id}}" #{id}`.strip
-	table_name = "#{HOSTNAME}_#{name}"
-	# "-" characters are not allowed in names, so we'll replace them with "_"
-	table_name = table_name.gsub('-', "_")
-	# and make sure it's downcased
-	table_name = table_name.downcase
-	if existing_tables.includes?(table_name)
-		puts "#{OK} - The table \"#{table_name}\" already exists"
+# we run this part only if the PROCESS_DOCKER_LOGS option
+# in the settings file is set to true
+if SETTINGS["PROCESS_DOCKER_LOGS"] == true 
+
+	puts "#{INFO} - Get list of containers..."
+	containers=`docker ps --all --format "{{.ID}} {{.Names}}"`.strip.split("\n")
+	if $?.exit_status != 0
+	        puts "#{FAIL} - Sorry, something went wrong while trying to get the container listing"
+	        exit 1
 	else
-		puts "#{INFO} - Creating the table \"#{table_name}\"..."
-		db.exec "create table #{table_name} (name varchar(2000), ts timestamptz)"
+	        puts "#{OK} - Got #{containers.size} containers."
 	end
 
-	log_filename="#{DOCKER_ROOT_DIR}/containers/#{full_id}/#{full_id}-json.log"
-	if File.file?(log_filename)
-		puts "#{INFO} - Found the file \"#{log_filename}\""
-		results=get_new_lines_from_file(log_filename,name)
-                next if results.none?
 
-		file_to_process  = results[0]
-		lines_to_process = results[1]
-		process_file(file_to_process,lines_to_process,db,table_name)
-		
-	else
-		puts "#{FAIL} - Sorry I could not find the file \"#{log_filename}\""
+	puts "#{INFO} - Getting the container logs..."
+	containers.each do |container|
+		id         = container.split(" ")[0]
+		name       = container.split(" ")[1]
+		full_id    = `docker inspect --format="{{.Id}}" #{id}`.strip
+		table_name = "#{HOSTNAME}_#{name}"
+		# "-" characters are not allowed in names, so we'll replace them with "_"
+		table_name = table_name.gsub('-', "_")
+		# and make sure it's downcased
+		table_name = table_name.downcase
+		if existing_tables.includes?(table_name)
+			puts "#{OK} - The table \"#{table_name}\" already exists"
+		else
+			puts "#{INFO} - Creating the table \"#{table_name}\"..."
+			db.exec "create table #{table_name} (name varchar(2000), ts timestamptz)"
+		end
+
+		log_filename="#{docker_Root_Dir}/containers/#{full_id}/#{full_id}-json.log"
+		if File.file?(log_filename)
+			puts "#{INFO} - Found the file \"#{log_filename}\""
+			results=get_new_lines_from_file(log_filename,name)
+	                next if results.none?
+
+			file_to_process  = results[0]
+			lines_to_process = results[1]
+			process_file(file_to_process,lines_to_process,db,table_name)
+			
+		else
+			puts "#{FAIL} - Sorry I could not find the file \"#{log_filename}\""
+		end
 	end
+
 end
--- docker_logs_startup_checks.cr
+++ docker_logs_startup_checks.cr
@@ -1,44 +1,6 @@
 module DockerLogsStartupChecks
 	extend self
 
-	# check if we can get the settings file
-	def check_if_settings_file_exists(settings_file)
-		puts "#{INFO} - Check if we can get the \"#{settings_file}\" file..."
-		if File.file?(settings_file)
-		        puts "#{OK} - The file \"#{settings_file}\" exists"
-		else
-		        puts "#{FAIL} - Sorry, I couldn't find the file \"#{settings_file}\"."
-		        exit 1
-		end
-
-	end
-
-	# check if a some shell command exists
-	# we use this to check if things like docker and logtail2 are installed
-	def check_for_shell_command(some_command)
-	        puts "#{INFO} - Check if the \"#{some_command}\" command exists..."
-	        begin
-	                Process.run(some_command)
-	                puts "#{OK} - the \"#{some_command}\" command has been found"
-	        rescue
-	                puts "#{FAIL} - Sorry the \"#{some_command}\" command could not be found"
-	                exit 1
-	        end
-	end
-
-	# check if we can get the hostname from the local machine
-	def get_hostname
-		puts "#{INFO} - Check if we can get the hostname..."
-		hostname=System.hostname
-		if hostname.empty?
-		        puts "#{FAIL} - Sorry, I cannot get the hostname :("
-		        exit 1
-		else
-		        puts "#{OK} - I got the hostname: \"#{hostname}\""
-		end
-		return hostname
-	end
-
 	# check if the docker service is running
 	def check_if_the_docker_service_is_running
 		puts "#{INFO} - Check if the docker service is running..."
